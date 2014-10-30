81b9fdf6e26bd93cd091afa3178818af17b51065
Randi Øyri <roy@difi.no>
2014-10-30 12:40:04 +0100

    PBLEID-7929. Endre på rapportering av start og slutt-tid. Rapporteringa feila dersom ein exception blei kasta. Laga meir robust kode + refactored.

--- kontaktinfo-app-wsclient-sample/src/main/java/no/difi/kontaktinfo/external/client/cxf/JMeterSampler.java
+++ kontaktinfo-app-wsclient-sample/src/main/java/no/difi/kontaktinfo/external/client/cxf/JMeterSampler.java
@@ -1,7 +1,6 @@
 package no.difi.kontaktinfo.external.client.cxf;
 
 
-
 import com.cedarsoftware.util.io.JsonReader;
 import no.difi.begrep.Person;
 import no.difi.kontaktinfo.wsdl.oppslagstjeneste_14_05.Oppslagstjeneste1405;
@@ -31,6 +30,7 @@ public class JMeterSampler extends AbstractJavaSamplerClient implements Serializ
 
         return defaultParameters;
     }
+
     @Override
     public SampleResult runTest(JavaSamplerContext javaSamplerContext) {
 
@@ -39,73 +39,102 @@ public class JMeterSampler extends AbstractJavaSamplerClient implements Serializ
         String behov = javaSamplerContext.getParameter("informasjonsbehov");
         String alias = javaSamplerContext.getParameter("alias");
 
-
-
-        System.out.println("called sampler " + String.format("%s,%s,%s,%s", endpoint, ssn, behov, alias) );
+        System.out.println("called sampler " + String.format("%s,%s,%s,%s", endpoint, ssn, behov, alias));
 
         SampleResult result = new SampleResult();
-        try{
-            Map<String, String> prop= new HashMap<String, String>();
-            prop.put(WSHandlerConstants.SIG_KEY_ID, "X509KeyIdentifier");
-            prop.put(WSHandlerConstants.SIGNATURE_PARTS, "{}{http://schemas.xmlsoap.org/soap/envelope/}Body;{}{http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd}Timestamp}");
-            OppslagstjenestenKlient oppslagstjenestenKlient = new OppslagstjenestenKlient(endpoint, alias, prop);
+        try {
+            OppslagstjenestenKlient oppslagstjenestenKlient = new OppslagstjenestenKlient(endpoint, alias, createWSConfigForV4());
             Oppslagstjeneste1405 kontaktinfoPort = oppslagstjenestenKlient.getOppslagstjenstePort();
 
-
             HentPersonerForespoersel req = new HentPersonerForespoersel();
-
-            for(String behovStreng: behov.split(",")){
-                Informasjonsbehov b = Informasjonsbehov.valueOf(behovStreng);
-                req.getInformasjonsbehov().add(b);
-            }
-
+            setInformasjonsbehov(behov, req);
 
             Object[] list = (Object[]) JsonReader.jsonToJava(ssn);
-            Map<String, Object[]> map = new HashMap<String, Object[]>();
+            final Map<String, Object[]> map = mapSsnTilConfig(req, list);
 
-            for(Object data : list){
-                Object[] listOfData = (Object[]) data;
-                String s = (String) listOfData[0];
-                req.getPersonidentifikator().add(s);
-                map.put(s, (Object[])data);
+
+            HentPersonerRespons hentPersonerRespons = executeHentPersoner(ssn, behov, alias, result, kontaktinfoPort, req);
+
+            if (hentPersonerRespons == null || hentPersonerRespons.getPerson() == null) {
+                result.setSuccessful(false);
+            } else if (map.size() != hentPersonerRespons.getPerson().size()) {
+                result.setSuccessful(false);
+            } else {
+                validerRespons(result, map, hentPersonerRespons);
             }
 
-            result.setSamplerData(alias +"@"+ behov + ": " + ssn);
-            result.sampleStart();
-            HentPersonerRespons hentPersonerRespons = kontaktinfoPort.hentPersoner(req);
-            result.sampleEnd();
-
-            int i = 0;
-            for(Person p : hentPersonerRespons.getPerson())
-            {
-                boolean havePostkasse = map.get(p.getPersonidentifikator())[2].equals("1");
-                System.out.println(p.getPersonidentifikator());
-                if(!p.getKontaktinformasjon().getEpostadresse().getValue().contains(p.getPersonidentifikator()))
-                    result.setSuccessful(false);
-
-                System.out.println(p.getSikkerDigitalPostAdresse());
-                if(havePostkasse){
-                    result.setSuccessful(p.getSikkerDigitalPostAdresse() != null);
-                }else{
-                    result.setSuccessful(p.getSikkerDigitalPostAdresse() == null);
-                }
-
-                if(result.isSuccessful()){
-                    result.setResponseMessage(p.getKontaktinformasjon().getEpostadresse().getValue());
-                    result.setSuccessful(true);
-                }
-
-
-            }
-
-
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-            result.setSuccessful(false);
+        } finally {
+            if (result.getStartTime() == 0l) {
+                result.sampleStart();
+                result.setSuccessful(false);
+            }
         }
 
         System.out.println("called sampler done!");
         return result;
 
+    }
+
+    private Map<String, String> createWSConfigForV4() {
+        Map<String, String> prop = new HashMap<String, String>();
+        prop.put(WSHandlerConstants.SIG_KEY_ID, "X509KeyIdentifier");
+        prop.put(WSHandlerConstants.SIGNATURE_PARTS, "{}{http://schemas.xmlsoap.org/soap/envelope/}Body;{}{http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd}Timestamp}");
+        return prop;
+    }
+
+    private Map<String, Object[]> mapSsnTilConfig(HentPersonerForespoersel req, Object[] list) {
+        Map<String, Object[]> map = new HashMap<String, Object[]>();
+        for (Object data : list) {
+            Object[] listOfData = (Object[]) data;
+            String s = (String) listOfData[0];
+            req.getPersonidentifikator().add(s);
+            map.put(s, (Object[]) data);
+        }
+        return map;
+    }
+
+    private void setInformasjonsbehov(String behov, HentPersonerForespoersel req) {
+        for (String behovStreng : behov.split(",")) {
+            Informasjonsbehov b = Informasjonsbehov.valueOf(behovStreng);
+            req.getInformasjonsbehov().add(b);
+        }
+    }
+
+    private HentPersonerRespons executeHentPersoner(String ssn, String behov, String alias, SampleResult result, Oppslagstjeneste1405 kontaktinfoPort, HentPersonerForespoersel req) {
+        HentPersonerRespons hentPersonerRespons;
+        try {
+            result.setSamplerData(alias + "@" + behov + ": " + ssn);
+            result.sampleStart();
+            hentPersonerRespons = kontaktinfoPort.hentPersoner(req);
+        } finally {
+            result.sampleEnd();
+        }
+        return hentPersonerRespons;
+    }
+
+    private void validerRespons(SampleResult result, Map<String, Object[]> map, HentPersonerRespons hentPersonerRespons) {
+        int i = 0;
+        for (Person p : hentPersonerRespons.getPerson()) {
+            boolean havePostkasse = map.get(p.getPersonidentifikator())[2].equals("1");
+            System.out.println(p.getPersonidentifikator());
+            if (!p.getKontaktinformasjon().getEpostadresse().getValue().contains(p.getPersonidentifikator()))
+                result.setSuccessful(false);
+
+            System.out.println(p.getSikkerDigitalPostAdresse());
+            if (havePostkasse) {
+                result.setSuccessful(p.getSikkerDigitalPostAdresse() != null);
+            } else {
+                result.setSuccessful(p.getSikkerDigitalPostAdresse() == null);
+            }
+
+            if (result.isSuccessful()) {
+                result.setResponseMessage(p.getKontaktinformasjon().getEpostadresse().getValue());
+                result.setSuccessful(true);
+            }
+
+
+        }
     }
 }
